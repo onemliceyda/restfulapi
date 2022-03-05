3c1055842c94ea785f315541158d608aa9ff2529
Ceyda <ceydaonemli2@gmail.com>
2022-03-05 15:04:41 +0300

    delete ve patch işlemleri gerçekleştirildi

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,8 +2,8 @@
 const User=require('../models/userModel')
 const router=require('express').Router(); ;
 
-router.get('/',(req,res)=>{
-    const tumUserlar= User.find({}).then();
+router.get('/',async(req,res)=>{
+    const tumUserlar=await User.find({});
     res.json(tumUserlar)
 })
 
@@ -16,19 +16,47 @@ router.get('/:id',(req,res)=>{
 
 
 
-router.post('/',(req,res)=>{
-    res.json(req.body)
+router.post('/',async(req,res)=>{
+
+    try {
+    const eklenecekUser=new User(req.body);
+    const sonuc=await eklenecekUser.save();
+    res.json(sonuc);
+    }catch(err){
+        console.log("kaydederken oluşan hata : "+err)
+    }
 })
 
 //güncelleme işlemleri için
-router.patch('/:id',(req,res)=>{
-    res.json({
-        mesaj:"idsi"+req.params.id+" olan kullanıcının bilgileri "+(req.body)+" bilgileriyle güncellenecek"
-    })
-})
+router.patch('/:id',async(req,res)=>{
+    try {
+        const sonuc= await User.findByIdAndUpdate({_id:req.params.id},req.body,{new:true,runValidators:true});
+        if(sonuc){
+           return res.json({sonuc});
+        }else{
+            return res.status(404).json({mesaj:"Kullanıcı bulunamadı"})
+        }
 
-router.delete('/:id',(req,res)=>{
-    res.json({mesaj: "id'si "+req.params.id+ " olan kullanıcı sistemden silinecek "})
+    } catch (error) {
+        console.log("Karşılaşılan hata : " +error)
+    }
+});
+
+router.delete('/:id',async(req,res)=>{
+    try {
+        const silinecekUser=await User.findByIdAndDelete({_id:req.params.id})
+        
+        if(silinecekUser){
+            return res.json({mesaj:"Kullanıcı silindi"})
+        }else{
+            return res.status(404).json({
+                mesaj:"Kullanıcı bulunamadı",
+            })
+        }
+    } catch (err) {
+        console.log("db de oluşan hata : "+err);
+    }
+
 })
 
 
